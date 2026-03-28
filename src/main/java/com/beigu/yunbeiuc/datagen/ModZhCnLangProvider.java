89489c0ebb797dca07ebe442a337e1f978b0e494
BGDST <HELLOBGSDT@QQ.COM>
2026-03-28 12:49:06 +0800

    修复bug，完善signs

--- src/main/java/com/beigu/yunbeiuc/datagen/ModZhCnLangProvider.java
+++ src/main/java/com/beigu/yunbeiuc/datagen/ModZhCnLangProvider.java
@@ -253,6 +253,8 @@ public class ModZhCnLangProvider extends FabricLanguageProvider {
         translationBuilder.add(ModBlocks.ROAD_POLE_TEXT_DISPLAY, "道路杆件文字显示屏");
         translationBuilder.add(ModBlocks.ROAD_POLE_FLAG, "道路杆件旗帜");
 
+
+
         translationBuilder.add("itemGroup.yunbeiuc_rb_group","云北城建 | 道路方块");
         translationBuilder.add("itemGroup.yunbeiuc_sings_group","云北城建 | 道路标识");
     }
